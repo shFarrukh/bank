5556c9ff4469d66f4b45bde03e8c4e0191f79c25
Farrukh <sh.farrukh99@mail.ru>
2021-04-05 18:37:28 +0300

    was Crated tags and branches

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -1,11 +1,24 @@
-package types 
-//money is kept in minimum currencies (cent kopeck diram) 
+package types
+
+//money is kept in minimum currencies (cent kopeck diram)
 type Money int64
+
 //payment categories (foods , clothes...)
 type Category string
+
+type Status string
+
+//Предопределенный статус платежей
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInPrigress Status = "INPROGRESS"
+)
+
 // Paymant information
-type Payment struct{
-	ID int
-	Amount Money
+type Payment struct {
+	ID       int
+	Amount   Money
 	Category Category
+	Status   Status
 }
