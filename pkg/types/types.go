4b55c8dbbcde8aa6eeefcc56b9a5b45d248f1908
Farrukh <sh.farrukh99@mail.ru>
2021-04-06 09:16:58 +0300

    Task 11.2! replaced types.go

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -4,8 +4,19 @@ type Money int64
 //payment categories (foods , clothes...)
 type Category string
 // Paymant information
+
+
+type Status string 
+
+const(
+	StatusOk Status ="OK"
+	StatusFail Status ="FAIL"
+	StatusInPrigress Status ="INPROGRESS"
+
+)
 type Payment struct{
 	ID int
 	Amount Money
 	Category Category
+	Status Status
 }
